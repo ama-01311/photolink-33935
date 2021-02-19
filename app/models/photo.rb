3489a1a313570f07d6ae7d25aeca5e23a3f31d24
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-19 22:13:41 +0900

    バリデーション記述

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,17 @@ class Photo < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :photo_category
   belongs_to :photo_place
+
+  with_options presence: true do
+    validates :photo_title
+    validates :photo_category_id
+    validates :photo_place_id
+    validates :photo_description
+    validates :selling_price
+  end
+  
+  with_options numericality: { other_than: 1 } do
+    validates :photo_category_id
+    validates :photo_place_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :photos
+
+  with_options presence: true do
+    validates :nickname
+    validates :lname
+    validates :fname
+    validates :lname_kana
+    validates :fname_kana
+    validates :birth_date
+  end
 end
