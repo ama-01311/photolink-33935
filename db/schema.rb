b27ed4982d444b6898afae7f2513218f4647cf3d
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-19 11:25:15 +0900

    photoモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_061106) do
+ActiveRecord::Schema.define(version: 2021_02_19_020930) do
+
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "photo_title", null: false
+    t.text "photo_description", null: false
+    t.integer "photo_category_id", null: false
+    t.integer "photo_place_id", null: false
+    t.integer "selling_price", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_02_18_061106) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "users"
 end
