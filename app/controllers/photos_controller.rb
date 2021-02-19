8ad191a287a4ad21088a94aec6a3eb2b67ef5196
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-19 22:14:20 +0900

    ルーティング・ビュー・コントローラ記述

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,22 @@ class PhotosController < ApplicationController
   def index
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def photo_params
-    params.require(:photo).permit(:image).merge(user_id: current_user.id)
+    params.require(:photo)
+          .permit(:image, :photo_title, :photo_description, :photo_category_id, :photo_place_id, :selling_price)
+          .merge(user_id: current_user.id)
   end
 end
